046ec1d5d3af9023d5a5fd5287cdeb912775ef23
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-01-31 17:49:59 -0300

    Starting class 3

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-const Modal = {
-    openModal() {
-        document.querySelector('.modal-overlay').classList.add('active')
-    },
-
-    closeModal() {
-        document.querySelector('.modal-overlay').classList.remove('active')
-    }
-}
