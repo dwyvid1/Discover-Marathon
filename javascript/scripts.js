e35510cd259f2bf9ca93b912f4759c91871d13ab
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-04 18:11:06 -0300

    Continuing class 3

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,38 +8,46 @@ const Modal = {
     }
 }
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    },
-    {
-        id: 4,
-        description: 'App',
-        amount: 200000,
-        date: '23/01/2021'
-    },
-]
-
 const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'App',
+            amount: 200000,
+            date: '23/01/2021'
+        },
+    ],
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
     incomes() {
         let income = 0
 
-        transactions.forEach(transaction => {
+        Transaction.all.forEach(transaction => {
             if (transaction.amount > 0) {
                 income += transaction.amount
             }
@@ -47,10 +55,11 @@ const Transaction = {
 
         return income
     },
+
     expenses() {
         let expense = 0
 
-        transactions.forEach(transaction => {
+        Transaction.all.forEach(transaction => {
             if (transaction.amount < 0) {
                 expense += transaction.amount
             }
@@ -58,6 +67,7 @@ const Transaction = {
 
         return expense
     },
+
     total() {
         return Transaction.incomes() + Transaction.expenses()
     }
@@ -101,6 +111,10 @@ const DOM = {
         document
             .getElementById('totalDisplay')
             .innerHTML = Utils.foormatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
@@ -121,8 +135,30 @@ const Utils = {
     }
 }
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+const Form = {
+    validateFields() {
+        console.log('Validar os campos')
+    },
 
-DOM.updateBalance()
+    submit(event) {
+        event.preventDefault()
+
+        Form.validateFields()
+    }
+}
+
+const App = {
+    init() {
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
