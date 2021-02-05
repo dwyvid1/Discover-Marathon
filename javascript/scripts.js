5c9b1b1cf249f02278906f23ada038e8db2eb573
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-05 18:43:55 -0300

    Finalizing the project :rocket:

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,29 +8,18 @@ const Modal = {
     }
 }
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
+
 const Transaction = {
-    all: [
-        {
-            description: 'Luz',
-            amount: -50000,
-            date: '23/01/2021'
-        },
-        {
-            description: 'Website',
-            amount: 500000,
-            date: '23/01/2021'
-        },
-        {
-            description: 'Internet',
-            amount: -20000,
-            date: '23/01/2021'
-        },
-        {
-            description: 'App',
-            amount: 200000,
-            date: '23/01/2021'
-        },
-    ],
+    all: Storage.get(),
 
     add(transaction) {
         Transaction.all.push(transaction)
@@ -215,12 +204,14 @@ const Form = {
 
 const App = {
     init() {
-        Transaction.all.forEach((transaction, index) => {
-            DOM.addTransaction(transaction, index)
-        })
-        
-        DOM.updateBalance()
+       Transaction.all.forEach(DOM.addTransaction)
+
+       DOM.updateBalance()
+
+       Storage.set(Transaction.all)
+
     },
+
     reload() {
         DOM.clearTransactions()
         App.init()
