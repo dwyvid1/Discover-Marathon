dab41816143c888558cc496e8219e9772abf0331
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-08 10:28:38 -0300

    Resolving a small bug

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -110,10 +110,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value) * 100
-        // value = Number(value.replace(/\,\./g, ""))
+        value = value * 100
 
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
